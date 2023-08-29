9e0675f98792953b26f866495afae1bff33e95bf
Loris QUESADO <loris.quesado@oclock.school>
2023-08-29 15:01:19 +0200

    Feat: Création d'un contect pour la génération des boutons de génération

---
+++ src/components/context/GenerationsButtons.tsx
@@ -0,0 +1,35 @@
+import { ReactElement, createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { Generation } from '../../@types';
+
+const GenerationsButtonsContext = createContext<Generation[]>([]);
+
+function GenerationsButtonsProvider({ children }: { children: ReactElement }) {
+  const [generationBtns, setGenerationBtns] = useState<Generation[]>([]);
+
+  useEffect(() => {
+    async function fetchGenerationsButtons() {
+      try {
+        const { data } = await axios.get(
+          'https://api-pokemon-fr.vercel.app/api/v1/gen'
+        );
+
+        setGenerationBtns(data);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    }
+
+    fetchGenerationsButtons();
+  }, []);
+
+  return (
+    <GenerationsButtonsContext.Provider value={generationBtns}>
+      {children}
+    </GenerationsButtonsContext.Provider>
+  );
+}
+
+export { GenerationsButtonsContext, GenerationsButtonsProvider };
